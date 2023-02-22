7efb0ff466dc0bc7c8219bf22856e732b8cf0f42
Subas Roy <53431194+Subas-roy@users.noreply.github.com>
2023-02-22 21:47:19 +0600

    added practice 1

--- C/Practice 34.5/practice 1.c
+++ C/Practice 34.5/practice 1.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    
+    FILE *inputFile;
+    FILE *outputFile;
+    inputFile = fopen("input.txt", "r");
+    if(inputFile==NULL)
+    {
+        printf("File not found\n");
+        return 0;
+    }
+    outputFile = fopen("output.txt", "w");
+
+    int a, b, c, d, e;
+    fscanf(inputFile, "%d %d %d %d %d", &a,&b,&c,&d,&e);
+    fprintf(outputFile, "%d %d %d %d %d\n", a,b,c,d,e);
+
+    fclose(inputFile); // close open files
+    fclose(outputFile);
     return 0;
 }
